8b257985169c2fb724ff5a04eac172eef5fecf16
Erick de Cássio <erickebb15@gmail.com>
2024-08-30 13:19:27 +0100

    Minor changes on Banner

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import brand from '../images/MainPage/storeBrand.jpg'
+import React from "react";
+import brand from "../images/MainPage/storeBrand.jpg";
 
-function Banner (props){
-    return (
-        <div className='banner'>
-            <img src={brand} alt="Banner da loja" /> 
-            <div className='banner-content'>
-                <h3 className='p-2 fw-semibold'>Bem vindo à nossa loja!</h3>
-                <h5>Entregamos para todo o país</h5>
-            </div>
-        </div>
-    );
+function Banner(props) {
+  return (
+    <div className="banner">
+      <img src={brand} alt="Banner da loja" />
+      <div className="banner-content">
+        <h3 className="p-2 fw-semibold">Bem vindo à nossa loja!</h3>
+        <h5>
+          Caso não encontre o produto que procura entre em contato conosco
+        </h5>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
